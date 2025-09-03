b5addc06320cdf0c2aa5c0b2a6750faba0f50463
Major Khan <jahan.a@growshareus.com>
2025-09-03 19:16:56 +0000

    main page

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,5 @@
 // vite.config.js
-
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
 export default defineConfig({
   plugins: [react()],
-  // Change this line
-  base: '/tennis/',
+  base: '/',
 })
