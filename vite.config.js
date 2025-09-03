8b0c03dabd7cfb51c6142b218e40e59a7d56f0d1
Major Khan <jahan.a@growshareus.com>
2025-09-03 19:09:02 +0000

    fix: set correct base path for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,10 @@
 // vite.config.js
+
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  base: '/', // Must be exactly this for a custom domain
+  // Change this line
+  base: '/tennis/',
 })
